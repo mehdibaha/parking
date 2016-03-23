8649df8246c034869a7bddd2413f406718054c86
Ruben Pericas <Ruben Pericas>
2016-03-23 16:56:07 +0100

    Attached Sortie to mps

--- Sortie.cpp
+++ Sortie.cpp
@@ -13,6 +13,7 @@
 
 //------------------------------------------------------ Include personnel
 #include "Sortie.h"
+#include "Config.h"
 
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
@@ -22,24 +23,20 @@
 //---------------------------------------------------- Variables statiques
 
 //------------------------------------------------------ Fonctions privées
-//static type nom ( liste de paramètres )
-// Mode d'emploi :
-//
-// Contrat :
-//
-// Algorithme :
-//
-//{
-//} //----- fin de nom
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
-void Sortie(	int parkingID, int compteurVoituresID, int nombrePlacesOccupeesID,
-				int requeteID, int semID, int numSemRequete )
+void Sortie( int parkingID, int nombrePlacesOccupeesID, int* requetesID, int nbRequetes )
 // Algorithme :
 //
 {
 	// INITIALISATION
+	// Attachement aux mps
+	struct requeteEntree* requeteEGB = shmat(requetesID[REQ_GB], NULL, NULL);
+	struct requeteEntree* requeteEBP_profs = shmat(requetesID[REQ_BP_PROFS], NULL, NULL);
+	struct requeteEntree* requeteEGB_autres = shmat(requetesID[REQ_BP_AUTRES], NULL, NULL);
+	struct placeParking* parking = shmat(parkingID, NULL, NULL);
+	int* nbPlacesOccupees = shmat(nombrePlacesOccupeesID, NULL, NULL);
 	
 	// MOTEUR
 	
