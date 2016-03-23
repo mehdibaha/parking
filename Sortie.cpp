dd36b6e4f93e83c0f69f17d6e8b1d389b40c1241
Ruben Pericas <Ruben Pericas>
2016-03-23 16:38:07 +0100

    Added Sortie.cpp file

---
+++ Sortie.cpp
@@ -0,0 +1,77 @@
+/*************************************************************************
+                           Sortie  -  description
+                             -------------------
+    début                :
+    copyright            : (C) par
+    e-mail               :
+*************************************************************************/
+
+//---------- Réalisation de la tâche <Sortie> (fichier Sortie.cpp) ---
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include "Sortie.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions privées
+//static type nom ( liste de paramètres )
+// Mode d'emploi :
+//
+// Contrat :
+//
+// Algorithme :
+//
+//{
+//} //----- fin de nom
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+void Sortie(	int parkingID, int compteurVoituresID, int nombrePlacesOccupeesID,
+				int requeteID, int semID, int numSemRequete )
+// Algorithme :
+//
+{
+	// INITIALISATION
+	
+	// MOTEUR
+	
+	// DESTRUCTION
+	
+} //----- fin de Nom
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
